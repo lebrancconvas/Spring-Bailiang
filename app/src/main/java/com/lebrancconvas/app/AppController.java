a46e8ad07ff4a8f81127c972f9418e734683f768
lebrancconvas <lebrancconvas@gmail.com>
2023-01-23 21:35:45 +0700

    ♻️ refactor: change the way to import

--- app/src/main/java/com/lebrancconvas/app/AppController.java
+++ app/src/main/java/com/lebrancconvas/app/AppController.java
@@ -1,6 +1,7 @@
 package com.lebrancconvas.app;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
